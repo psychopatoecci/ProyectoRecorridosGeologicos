4d2b1d42f39088e3124cd7c006cde321ee972deb
JoseDanielSC <josedaniel2214@gmail.com>
2017-04-23 18:38:51 -0600

    Nuevo controller Pages
    Este controlador maneja ahora la lógica principal

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Pages Controller
+ *
+ * @property \App\Model\Table\PagesTable $Pages */
+class PagesController extends AppController
+{
+
+    /**
+     * Home method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function home()
+    {
+    }
+	
+	    /**
+     * Information method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function information()
+    {
+		
+    }
+	
+	/**
+     * Gallery method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function gallery()
+    {
+		
+    }
+	
+		    /**
+     *Contact method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function contact()
+    {
+		
+    }
+}
