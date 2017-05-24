669ab0f9ec0cd70ce6fd68bb55a2fdb6c30b296b
Isabel Chaves <ichaves94@gmail.com>
2017-05-24 09:23:02 -0600

    View docs instead download

--- Scripts para la base de datos/Script.sql
+++ Scripts para la base de datos/Script.sql
@@ -183,8 +183,8 @@ INSERT INTO contents VALUES(54,'P20R1','../../resources/travel/maps/1/Punto19-8.
 -- tourSantaElena
 INSERT INTO contents VALUES (19, 'tourSantaElena', '../resources/travel/santa_elena/santa_elena.png', '', 'image', 0);
 INSERT INTO contents VALUES (20, 'tourSantaElena', '../resources/travel/santa_elena/santa_elena.txt','La Península de Santa Elena se encuentra compuesta por rocas ígneas y sedimentarias. Las primeras corresponden a peridotitas formadas en el manto superior de la Tierra, mientras que, las últimas son calizas e intercalaciones de areniscas y lutitas, las formaciones de El Viejo, Santa Ana, Curú y Descartes, pertenecen respectivamente a esos tipos de roca generadas en el talud continental, contenidas en una historia geológica de más de 100 millones de años, culminando en Isla Bolaños.', 'text', 0);
-INSERT INTO contents VALUES (21, 'tourSantaElena', 'http://revistes.ub.edu/index.php/GEOACTA/article/download/105.000000365/4199','Magmatic and geotectonic significance of Santa Elena Peninsula, Costa Rica', 'url', 0);
-INSERT INTO contents VALUES (22, 'tourSantaElena', 'http://revistes.ub.edu/index.php/GEOACTA/article/download/105.000000364/4198','Evidence for middle Cretaceous accretion at Santa Elena Peninsula (Santa Rosa Accretionary Complex), Costa Rica', 'url', 1);
+INSERT INTO contents VALUES (21, 'tourSantaElena', 'http://revistes.ub.edu/index.php/GEOACTA/article/view/105.000000365/4199','Magmatic and geotectonic significance of Santa Elena Peninsula, Costa Rica', 'url', 0);
+INSERT INTO contents VALUES (22, 'tourSantaElena', 'http://revistes.ub.edu/index.php/GEOACTA/article/view/105.000000364/4198','Evidence for middle Cretaceous accretion at Santa Elena Peninsula (Santa Rosa Accretionary Complex), Costa Rica', 'url', 1);
 -- tourSantaElena contents
 INSERT INTO contents VALUES(55,'P1R2','../../resources/travel/maps/2/Punto1-1. Columna Estratigrafica.jpg','Columna Estratigrafica','image',0);
 INSERT INTO contents VALUES(56,'P1R2','../../resources/travel/maps/2/Punto1-2.jpg','','image',0);
