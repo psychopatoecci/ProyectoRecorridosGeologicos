0092037e116e95f6e0c5e6f2ae71858993217f38
NotSpooky <asistentedeprincipios2@gmail.com>
2017-06-28 06:44:05 -0600

    Agrega ruta que no subí

--- config/routes.php
+++ config/routes.php
@@ -58,6 +58,7 @@ Router::scope('/', function (RouteBuilder $routes) {
     $routes->connect('/pages/gallery/*', ['controller' => 'Pages', 'action' => 'gallery']);
     $routes->connect('/pages/contact', ['controller' => 'Pages', 'action' => 'contact']);
     $routes->connect('/pages/tour/*', ['controller' => 'MapPoints', 'action' => 'view']);
+    $routes->connect('/users/recover/*', ['controller' => 'Users', 'action' => 'recover']);
     
     
     /**
